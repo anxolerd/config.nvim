74afb4d66ae10b6ab9f9aa1d5879ff128b23b1d4
Oleksandr Kovalchuk <anxolerd@outlook.com>
2025-10-14 17:24:02 +0300

    Change description of ,l to have mnemonic

--- lua/neo/keymaps.lua
+++ lua/neo/keymaps.lua
@@ -1,2 +1,2 @@
 -- show all keybindings
-vim.keymap.set("n", "<leader>l", "<cmd>Telescope keymaps<cr>", { desc = "Show all keybindings" })
+vim.keymap.set("n", "<leader>l", "<cmd>Telescope keymaps<cr>", { desc = "[L]ist all keybindings" })
