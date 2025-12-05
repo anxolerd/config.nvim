8ea7441c9bbef3b870bee19e45970c929c6e3b74
Oleksandr Kovalchuk <anxolerd@outlook.com>
2025-12-05 18:59:51 +0200

    Update changes

--- lua/neo/plugins/lsp.lua
+++ lua/neo/plugins/lsp.lua
@@ -55,6 +55,7 @@ return { -- LSP Configuration & Plugins
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+      nmap('<leader>e', vim.diagnostic.open_float, 'Show Diagnostics [E]rrors')
 
       -- See `:help K` for why this keymap
       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -84,6 +85,7 @@ return { -- LSP Configuration & Plugins
       'ansiblels',
       'bashls',
       'jdtls',
+      'jsonnet_ls',
       'lua_ls',
       'pyright',
       'rust_analyzer',
